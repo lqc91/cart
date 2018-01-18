9092299f6b467f820ce736c572e897be0615d12b
lqc91 <1007669205@qq.com>
2018-01-18 23:10:02 +0800

    Add onblur event, and optimize addition and subtraction button click events

--- js/script.js
+++ js/script.js
@@ -33,7 +33,12 @@ for ( var i = 0; i < selLen; i ++ ) {
 				break;
 			}
 		}
-	}
+	};
+}
+
+// 计算一条商品总价
+function totalCal () {
+	return totalArr[index].innerHTML = "￥" + parseInt( priceArr[index].innerHTML.slice(1) ) * parseInt( countValue[index].value );
 }
 
 // 计算应付金额
@@ -49,7 +54,6 @@ function payCal () {
 	pay.innerHTML = "￥" + sum;
 }
 
-// 加减按钮点击事件
 var countAdd = document.querySelectorAll(".count-add");
 var countSubtract = document.querySelectorAll(".count-subtract");
 var countValue = document.querySelectorAll(".count-value");
@@ -57,21 +61,38 @@ var priceArr = document.querySelectorAll(".item-price");
 var index = 0;
 for(var i = 0; i < countValue.length; i ++ ) {
 	countAdd[i].id = "add-btn-" + i;
+	// 加号按钮点击事件：
+	// 点击加号按钮一次，对应商品数量加一，并改变相应商品总价与应付金额
 	countAdd[i].onclick = function () {
 		index = this.id.slice(8);
 		countValue[index].value = parseInt( countValue[index].value ) + 1;
-		totalArr[index].innerHTML = "￥" + parseInt( priceArr[index].innerHTML.slice(1) ) * parseInt( countValue[index].value );
+		totalCal();
 		payCal();
-	}
+	};
 	countSubtract[i].id = "subtract-btn-" + i;
+	// 减号按钮点击事件：
+	// 点击减号按钮一次，判断对应商品数量是否不小于1
+	// 若是，对应商品数量减一，并改变相应商品总价与应付金额；
+	// 若否，不进行任何操作
 	countSubtract[i].onclick = function () {
 		index = this.id.slice(13);
-		if ( countValue[index].value == 1 ) {
-			countValue[index].value = 1;
-		} else {
+		if ( countValue[index].value > 1 ) {
 			countValue[index].value -= 1;
+			totalCal();
+			payCal();
 		}
-		totalArr[index].innerHTML = "￥" + parseInt( priceArr[index].innerHTML.slice(1) ) * parseInt( countValue[index].value );
-		payCal();
+	};
+	countValue[i].id = "value-ipt-" + i;
+	// 鼠标失焦事件：
+	// 键盘输入商品数量，鼠标失焦，判断输入是否为不小于1的数字，并执行相应代码
+	countValue[i].onblur = function () {
+		index = this.id.slice(10);
+		// console.log(Number( countValue[index].value ));
+		if ( isNaN( Number( countValue[index].value ) ) || Number( countValue[index].value ) < 1 ) {
+			alert( "请输入正确的数字！" );
+		} else {
+			totalCal();
+			payCal();
+		}
 	}
 }
