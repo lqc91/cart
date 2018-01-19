e06cb612d62585a29692b4c51df1597dcdd1e2bc
lqc91 <1007669205@qq.com>
2018-01-20 00:20:37 +0800

    Encapsulate the settlement function

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-var cart = document.querySelector("#cart");
+// var cart = document.querySelector("#cart");
 var selAll = cart.querySelector(".sel-all-ipt");
 var sel = cart.querySelectorAll(".sel-ipt");
 var selLen = sel.length;
@@ -14,7 +14,7 @@ selAll.onclick = function () {
 		for ( var i = 0; i < selLen; i ++ ) {
 			sel[i].checked = true;
 		}
-		payCal();
+		settle();
 	}
 };
 
@@ -24,75 +24,75 @@ for ( var i = 0; i < selLen; i ++ ) {
 		for ( var i = 0; i < selLen; i++ ) {
 			if ( sel[i].checked === false ) {
 				selAll.checked = false;
-				payCal();
+				settle();
 				break;
 			}
 			if ( i === selLen-1 ) {
 				selAll.checked = true;
-				payCal();
+				settle();
 				break;
 			}
 		}
 	};
 }
 
-// 计算一条商品总价
-function totalCal () {
-	return totalArr[index].innerHTML = "￥" + parseInt( priceArr[index].innerHTML.slice(1) ) * parseInt( countValue[index].value );
-}
-
-// 计算应付金额
-var pay = document.querySelector("#pay");
-var totalArr = document.querySelectorAll(".item-total");
-function payCal () {
-	var sum = 0;
-	for ( var i = 0; i < totalArr.length; i++ ) {
-		 if ( sel[i].checked === true ) {
-		 	sum += parseInt(totalArr[i].innerHTML.slice(1));
-		 }
-	}
-	pay.innerHTML = "￥" + sum;
-}
-
-var countAdd = document.querySelectorAll(".count-add");
-var countSubtract = document.querySelectorAll(".count-subtract");
-var countValue = document.querySelectorAll(".count-value");
-var priceArr = document.querySelectorAll(".item-price");
-var index = 0;
-for(var i = 0; i < countValue.length; i ++ ) {
-	countAdd[i].id = "add-btn-" + i;
+var add = document.querySelectorAll(".count-add");
+var subtract = document.querySelectorAll(".count-subtract");
+var value = document.querySelectorAll(".count-value");
+var price = document.querySelectorAll(".item-price");
+var idx = 0;
+for(var i = 0; i < selLen; i ++ ) {
+	add[i].id = "add-btn-" + i;
 	// 加号按钮点击事件：
 	// 点击加号按钮一次，对应商品数量加一，并改变相应商品总价与应付金额
-	countAdd[i].onclick = function () {
-		index = this.id.slice(8);
-		countValue[index].value = parseInt( countValue[index].value ) + 1;
-		totalCal();
-		payCal();
+	add[i].onclick = function () {
+		idx = this.id.slice(8);
+		value[idx].value = parseInt( value[idx].value ) + 1;
+		settle();
 	};
-	countSubtract[i].id = "subtract-btn-" + i;
+	subtract[i].id = "subtract-btn-" + i;
 	// 减号按钮点击事件：
 	// 点击减号按钮一次，判断对应商品数量是否不小于1
 	// 若是，对应商品数量减一，并改变相应商品总价与应付金额；
 	// 若否，不进行任何操作
-	countSubtract[i].onclick = function () {
-		index = this.id.slice(13);
-		if ( countValue[index].value > 1 ) {
-			countValue[index].value -= 1;
-			totalCal();
-			payCal();
+	subtract[i].onclick = function () {
+		idx = this.id.slice(13);
+		if ( value[idx].value > 1 ) {
+			value[idx].value -= 1;
+			settle();
 		}
 	};
-	countValue[i].id = "value-ipt-" + i;
+	value[i].id = "value-ipt-" + i;
 	// 鼠标失焦事件：
 	// 键盘输入商品数量，鼠标失焦，判断输入是否为不小于1的数字，并执行相应代码
-	countValue[i].onblur = function () {
-		index = this.id.slice(10);
-		// console.log(Number( countValue[index].value ));
-		if ( isNaN( Number( countValue[index].value ) ) || Number( countValue[index].value ) < 1 ) {
+	value[i].onblur = function () {
+		idx = this.id.slice(10);
+		if ( isNaN( Number( value[idx].value ) ) || Number( value[idx].value ) < 1 ) {
 			alert( "请输入正确的数字！" );
 		} else {
-			totalCal();
-			payCal();
+			settle();
 		}
 	}
+}
+
+var pay = document.querySelector("#pay");
+var total = document.querySelectorAll(".item-total");
+var priceValue = [];
+var quantity = [];
+var totalValue = [];
+
+// 结算函数
+function settle () {
+	var sum = 0;
+	for ( var i = 0; i < selLen; i++ ) {
+		// console.log(price[i].innerHTML);
+		priceValue[i] = parseInt( price[i].innerHTML.slice(1) );
+		quantity[i] = parseInt( value[i].value );
+		totalValue[i] = priceValue[i] * quantity[i];
+		total[i].innerHTML = "￥" + totalValue[i];
+		if ( sel[i].checked === true ) {
+			sum += totalValue[i];
+		}
+	}
+	pay.innerHTML = "￥" + sum;
 }
