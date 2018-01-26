0f5fa864da28784bdfdec73a3ec9ed16fef73a16
lqc91 <1007669205@qq.com>
2018-01-27 01:55:06 +0800

    Revert "Name the functions"
    This reverts commit 8182f60bd49d785956e3cb78b4e9f8209468c534.

--- js/script.js
+++ js/script.js
@@ -5,25 +5,23 @@ var sel = cart.querySelectorAll(".sel-ipt");
 var selLen = sel.length;
 
 // 全选按钮全选与全不选事件
-selAll.onclick = selAllHandler;
-function selAllHandler () {
+selAll.onclick = function () {
 	if ( this.checked === false ) {
 		for ( var i = 0; i < selLen; i ++ ) {
 			sel[i].checked = false;
 		}
-		money.innerHTML = "￥" + 0;
+		pay.innerHTML = "￥" + 0;
 	} else {
 		for ( var i = 0; i < selLen; i ++ ) {
 			sel[i].checked = true;
 		}
 		settle();
 	}
-}
+};
 
 // 商品选择按钮选择与不选事件
 for ( var i = 0; i < selLen; i ++ ) {
-	sel[i].onclick = selHandler;
-	function selHandler () {
+	sel[i].onclick = function () {
 		for ( var i = 0; i < selLen; i++ ) {
 			if ( sel[i].checked === false ) {
 				selAll.checked = false;
@@ -36,48 +34,41 @@ for ( var i = 0; i < selLen; i ++ ) {
 				break;
 			}
 		}
-	}
+	};
 }
 
-var add = cart.querySelectorAll(".add");
-var subtract = cart.querySelectorAll(".subtract");
-var num = cart.querySelectorAll(".num");
-var price = cart.querySelectorAll(".j-price");
-// Uncaught TypeError: price.slice is not a function?
-// var price = cart.querySelectorAll(".price");
-// var priceArr = price.slice(1);
+var add = document.querySelectorAll(".count-add");
+var subtract = document.querySelectorAll(".count-subtract");
+var value = document.querySelectorAll(".count-value");
+var price = document.querySelectorAll(".item-price");
 var idx = 0;
 for(var i = 0; i < selLen; i ++ ) {
 	add[i].id = "add-btn-" + i;
 	// 加号按钮点击事件：
 	// 点击加号按钮一次，对应商品数量加一，并改变相应商品总价与应付金额
-	add[i].onclick = addHandler;
-	function addHandler () {
+	add[i].onclick = function () {
 		idx = this.id.slice(8);
-		num[idx].value = +num[idx].value + 1;
+		value[idx].value = parseInt( value[idx].value ) + 1;
 		settle();
-	}
+	};
 	subtract[i].id = "subtract-btn-" + i;
 	// 减号按钮点击事件：
 	// 点击减号按钮一次，判断对应商品数量是否不小于1
 	// 若是，对应商品数量减一，并改变相应商品总价与应付金额；
 	// 若否，不进行任何操作
-	subtract[i].onclick = subtractHandler;
-	function subtractHandler () {
+	subtract[i].onclick = function () {
 		idx = this.id.slice(13);
-		if ( num[idx].value > 1 ) {
-			num[idx].value -= 1;
+		if ( value[idx].value > 1 ) {
+			value[idx].value -= 1;
 			settle();
 		}
-	}
-	num[i].id = "num-ipt-" + i;
+	};
+	value[i].id = "value-ipt-" + i;
 	// 鼠标失焦事件：
 	// 键盘输入商品数量，鼠标失焦，判断输入是否为不小于1的数字，并执行相应代码
-	num[i].onblur = numHandler;
-	function numHandler () {
-		idx = this.id.slice(8);
-		console.log(idx);
-		if ( isNaN( +num[idx].value ) || +num[idx].value < 1 ) {
+	value[i].onblur = function () {
+		idx = this.id.slice(10);
+		if ( isNaN( Number( value[idx].value ) ) || Number( value[idx].value ) < 1 ) {
 			alert( "请输入正确的数字！" );
 		} else {
 			settle();
@@ -85,24 +76,24 @@ for(var i = 0; i < selLen; i ++ ) {
 	}
 }
 
-var money = document.querySelector(".money");
-var total = cart.querySelectorAll(".j-total");
-var priceVal = [];
-var numVal = [];
-var totalVal = [];
+var pay = document.querySelector("#pay");
+var total = document.querySelectorAll(".item-total");
+var priceValue = [];
+var quantity = [];
+var totalValue = [];
 
 // 结算函数
 function settle () {
 	var sum = 0;
 	for ( var i = 0; i < selLen; i++ ) {
 		// console.log(price[i].innerHTML);
-		priceVal[i] = +price[i].innerHTML.slice(1);
-		numVal[i] = +num[i].value;
-		totalVal[i] = priceVal[i] * numVal[i];
-		total[i].innerHTML = "￥" + totalVal[i];
+		priceValue[i] = parseInt( price[i].innerHTML.slice(1) );
+		quantity[i] = parseInt( value[i].value );
+		totalValue[i] = priceValue[i] * quantity[i];
+		total[i].innerHTML = "￥" + totalValue[i];
 		if ( sel[i].checked === true ) {
-			sum += totalVal[i];
+			sum += totalValue[i];
 		}
 	}
-	money.innerHTML = "￥" + sum;
+	pay.innerHTML = "￥" + sum;
 }
