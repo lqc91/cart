e9f4fe7d895777cf14612166fb274a4485c48361
lqc91 <1007669205@qq.com>
2018-01-28 20:32:28 +0800

    Group code, and fix select function bug

--- js/script.js
+++ js/script.js
@@ -1,108 +1,116 @@
 // 若将var cart = document.querySelector("#cart");注释，代码为何正常运行而不报错？
-var cart = document.querySelector("#cart");
-var selAll = cart.querySelector(".sel-all-ipt");
-var sel = cart.querySelectorAll(".sel-ipt");
-var selLen = sel.length;
+var cartWrap = document.querySelector("#cart");
+var cartElem = {
+	selAll: cartWrap.querySelector(".sel-all-ipt"),
+	sel: cartWrap.querySelectorAll(".sel-ipt"),
+	add: cartWrap.querySelectorAll(".add"),
+	subtract: cartWrap.querySelectorAll(".subtract"),
+	quantity: cartWrap.querySelectorAll(".quantity"),
+	price: cartWrap.querySelectorAll(".j-price"),
+	total: cartWrap.querySelectorAll(".j-total")
+};
+var money = document.querySelector(".money");
+var selLen = cartElem.sel.length;
+var idx = 0;
+// 运算
+var operation = {
+	// 累加
+	add: function() {
+		idx = this.id.slice(4);
+		cartElem.quantity[idx].value = +cartElem.quantity[idx].value + 1;
+		calMoney();
+	},
+	// 累减
+	subtract: function() {
+		idx = this.id.slice(9);
+		if ( +cartElem.quantity[idx].value > 1 ) {
+			cartElem.quantity[idx].value -= 1;
+			calMoney();
+		}
+	}
+};
 
 // 全选按钮全选与全不选事件
-selAll.onclick = selAllHandler;
+cartElem.selAll.onclick = selAllHandler;
 
-var add = cart.querySelectorAll(".add");
-var subtract = cart.querySelectorAll(".subtract");
-var quantity = cart.querySelectorAll(".quantity");
-var price = cart.querySelectorAll(".j-price");
-var idx = 0;
 for(var i = 0; i < selLen; i ++ ) {
 	// 商品选择按钮选择与不选事件
-	sel[i].onclick = selHandler;
-	
-	add[i].id = "add-" + i;
-	// 加号按钮点击事件：
-	// 点击加号按钮一次，对应商品数量加一，并改变相应商品总价与应付金额
-	add[i].onclick = addHandler;
-	
-	subtract[i].id = "subtract-" + i;
-	// 减号按钮点击事件：
-	// 点击减号按钮一次，判断对应商品数量是否不小于1
-	// 若是，对应商品数量减一，并改变相应商品总价与应付金额；
-	// 若否，不进行任何操作
-	subtract[i].onclick = subtractHandler;
-	
-	quantity[i].id = "quantity-" + i;
-	// 鼠标失焦事件：
-	// 键盘输入商品数量，鼠标失焦，判断输入是否为不小于1的数字，并执行相应代码
-	quantity[i].onblur = quantityHandler;
+	cartElem.sel[i].onclick = selHandler;
+	// 为加号按钮添加id
+	appendId(cartElem.add[i], i);
+	// 添加加号按钮点击事件
+	cartElem.add[i].onclick = operation.add;
+	// 为减号按钮添加id
+	appendId(cartElem.subtract[i], i);
+	// 添加减号按钮点击事件
+	cartElem.subtract[i].onclick = operation.subtract;
+	// 为数量输入框添加id
+	appendId(cartElem.quantity[i], i);
+	// 添加鼠标失焦事件
+	cartElem.quantity[i].onblur = quantity;
 }
 
-var money = document.querySelector(".money");
-var total = cart.querySelectorAll(".j-total");
-var priceValue = [];
-var quantityValue = [];
-var totalValue = [];
 // 全选/全不选
 function selAllHandler() {
 	if ( this.checked === false ) {
 		for ( var i = 0; i < selLen; i ++ ) {
-			sel[i].checked = false;
+			cartElem.sel[i].checked = false;
 		}
-		money.innerHTML = "￥" + 0;
 	} else {
 		for ( var i = 0; i < selLen; i ++ ) {
-			sel[i].checked = true;
+			cartElem.sel[i].checked = true;
 		}
-		settle();
 	}
+	calMoney();
 }
 // 选择
 function selHandler() {
-	for ( var i = 0; i < selLen; i++ ) {
-		if ( sel[i].checked === false ) {
-			selAll.checked = false;
-			settle();
-			break;
-		}
-		if ( i === selLen-1 ) {
-			selAll.checked = true;
-			settle();
-			break;
+	if ( this.checked === false ) {
+		cartElem.selAll.checked = false;
+		calMoney();
+	} else {
+		for ( var i = 0; i < selLen; i++ ) {
+			if (cartElem.sel[i].checked === false) {
+				calMoney();
+				break;
+			}
+			if (  i === selLen-1 ) {
+				cartElem.selAll.checked = true;
+				calMoney();
+				break;
+			}
 		}
 	}
 }
-// 累加
-function addHandler() {
-	idx = this.id.slice(4);
-	quantity[idx].value = +quantity[idx].value + 1;
-	settle();
-}
-// 累减
-function subtractHandler() {
+// 判断输入的数量是否合法
+function quantity() {
 	idx = this.id.slice(9);
-	if ( quantity[idx].value > 1 ) {
-		quantity[idx].value -= 1;
-		settle();
-	}
-}
-// 更新数量
-function quantityHandler() {
-	idx = this.id.slice(9);
-	if ( isNaN( +quantity[idx].value ) || +quantity[idx].value < 1 ) {
+	if ( isNaN( +cartElem.quantity[idx].value ) || +cartElem.quantity[idx].value < 1 ) {
 		alert( "请输入不小于1的正确数字！" );
-		quantity[idx].value = 1;
+		cartElem.quantity[idx].value = 1;
 	}
-	settle();
+	calMoney();
 }
-// 结算：计算应付金额
-function settle () {
+// 添加id
+function appendId(elem, index) {
+	return elem.id = elem.title + "-" + index;
+}
+// 计算总价与应付金额
+function calMoney () {
 	var sum = 0;
 	for ( var i = 0; i < selLen; i++ ) {
-		// console.log(price[i].innerHTML);
-		priceValue[i] = +price[i].innerHTML.slice(1);
-		quantityValue[i] = +quantity[i].value;
-		totalValue[i] = priceValue[i] * quantityValue[i];
-		total[i].innerHTML = "￥" + totalValue[i];
-		if ( sel[i].checked === true ) {
-			sum += totalValue[i];
+		updateMoney(cartElem.total[i], getMoney(cartElem.price[i]) * cartElem.quantity[i].value);
+		if ( cartElem.sel[i].checked === true ) {
+			sum += getMoney(cartElem.total[i]);
 		}
 	}
-	money.innerHTML = "￥" + sum;
+	updateMoney(money, sum);
+}
+// 获取金额的值
+function getMoney(elem) {
+	return +elem.innerHTML.slice(1);
+}
+// 更新金额
+function updateMoney(elem, num) {
+	return elem.innerHTML = "￥" + num;
 }
